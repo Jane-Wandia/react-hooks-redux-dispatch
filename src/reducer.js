2a9a40728d4c7e219312650c74df4c6f22ee3153
janekigenyi <janekigenyi@gmail.com>
2022-12-13 09:50:13 +0300

    completed

--- src/reducer.js
+++ src/reducer.js
@@ -7,7 +7,20 @@ function changeState(state, action) {
   }
 }
 
+
 let state = { count: 0 };
 let action = { type: "counter/increment" };
 
-changeState(state, action);
+function dispatch(action) {
+  state = changeState(state, action);
+  return state;
+}
+dispatch({ type: "counter/increment" });
+dispatch({ type: "counter/increment" });
+
+function render() {
+  document.body.textContent = state.count;
+}
+
+// call the render function
+render();
